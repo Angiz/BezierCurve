c654b812d5c06cc7474c46d4fb01125fba477191
crdiam <angiziewska@gmail.com>
2019-11-22 19:06:25 +0100

    Bezier Curve with degree of 1-3

--- BezierApp.py
+++ BezierApp.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+from math import *
 
 class MyForm(QMainWindow):
     def __init__(self):
@@ -16,7 +17,6 @@ class MyForm(QMainWindow):
     def getDegree(self):
         global degree
         degree =(int)(self.ui.polynomialText.text())
-        print(degree)
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -27,22 +27,45 @@ class MyForm(QMainWindow):
         settingPos = QtCore.QPoint(505, 145)
         settingPosX = settingPos.x()
         settingPosY = settingPos.y()
+
         for pos in self.chosenPoints:
             iter = iter+1
             if iter>degree+1:
                 break
             x = pos.x()
             y = pos.y()
-
             settingPosY = settingPosY+20
             qp.drawPoint(pos)
             qp.drawText(x-10, y-10, "P {}".format(iter))
             qp.drawText(settingPosX, settingPosY+20, "P {} {} ".format(iter, pos))
+            penLine = QPen(Qt.green, 4)
+            path = QPainterPath()
+            if (degree == 1) and (iter == 2):
+                qp.setPen(penLine)
+                qp.drawLine(self.chosenPoints[0].x(), self.chosenPoints[0].y(), self.chosenPoints[1].x(), self.chosenPoints[1].y())
+            elif (degree == 2) and (iter == 3):
+                qp.setPen(penLine)
+                path.moveTo(self.chosenPoints[0].x(), self.chosenPoints[0].y())
+                path.cubicTo(self.chosenPoints[0].x(), self.chosenPoints[0].y(), self.chosenPoints[1].x(), self.chosenPoints[1].y(), self.chosenPoints[2].x(), self.chosenPoints[2].y())
+                qp.drawPath(path)
+            elif (degree == 3) and (iter == 4):
+                qp.setPen(penLine)
+                path.moveTo(self.chosenPoints[0].x(), self.chosenPoints[0].y())
+                path.quadTo(self.chosenPoints[1].x(), self.chosenPoints[1].y(),  self.chosenPoints[3].x(), self.chosenPoints[3].y())
+                qp.drawPath(path)
+        qp.end()
 
+
+    def mousePressEvent(self, event):
+        if event.buttons() & QtCore.Qt.LeftButton:
+            print("Jakis tymczasowy tekst, dopoki nie bede miala pomyslu, co z tym zrobic")
 
     def mouseReleaseEvent(self, cursor_event):
         self.chosenPoints.append(cursor_event.pos())
         self.update()
+
+
+
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
