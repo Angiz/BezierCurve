9439e9f25f1e2432ea257014db5d49c14585cd84
crdiam <angiziewska@gmail.com>
2019-11-24 13:14:56 +0100

    Bezier Curve with constraints of lenght and clear button usage

--- BezierApp.py
+++ BezierApp.py
@@ -11,6 +11,7 @@ class MyForm(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.polynomialButton.clicked.connect(self.getDegree)
+        self.ui.clearButton.clicked.connect(self.Clearing)
         self.chosenPoints = []
         self.show()
 
@@ -18,11 +19,14 @@ class MyForm(QMainWindow):
         global degree
         degree =(int)(self.ui.polynomialText.text())
 
+    def Clearing(self):
+        self.ui.polynomialText.clear()
+        self.chosenPoints.clear()
+
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         pen = QPen(Qt.black, 8)
-        qp.setPen(pen)
         iter = 0
         settingPos = QtCore.QPoint(505, 145)
         settingPosX = settingPos.x()
@@ -35,6 +39,7 @@ class MyForm(QMainWindow):
             x = pos.x()
             y = pos.y()
             settingPosY = settingPosY+20
+            qp.setPen(pen)
             qp.drawPoint(pos)
             qp.drawText(x-10, y-10, "P {}".format(iter))
             qp.drawText(settingPosX, settingPosY+20, "P {} {} ".format(iter, pos))
@@ -50,20 +55,21 @@ class MyForm(QMainWindow):
                 qp.drawPath(path)
             elif (degree >2) and (iter >3):
                 path.moveTo(self.chosenPoints[0].x(), self.chosenPoints[0].y())
-
                 length = len(self.chosenPoints)
-                for i in range (1, (len(self.chosenPoints)-2)):
-                    p0 = self.chosenPoints[i]
-                    p1 = self.chosenPoints[i+1]
-                    midx = (p0.x() + p1.x()) / 2
-                    midy = (p0.y() + p1.y()) / 2
-                    path.quadTo(p0.x(), p0.y(), midx, midy)
-                    qp.setPen(penLine)
+                if length <= (degree + 1):
+                    for i in range (1, (length-2)):
+                        p0 = self.chosenPoints[i]
+                        p1 = self.chosenPoints[i+1]
+                        midx = (p0.x() + p1.x()) / 2
+                        midy = (p0.y() + p1.y()) / 2
+                        path.quadTo(p0.x(), p0.y(), midx, midy)
+                        qp.setPen(penLine)
+                        qp.drawPath(path)
+                    p0 = self.chosenPoints[length-2]
+                    p1 = self.chosenPoints[length-1]
+                    path.quadTo(p0.x(), p0.y(), p1.x(), p1.y())
                     qp.drawPath(path)
-                p0 = self.chosenPoints[length-2]
-                p1 = self.chosenPoints[length-1]
-                path.quadTo(p0.x(), p0.y(), p1.x(), p1.y())
-                qp.drawPath(path)
+                    qp.save()
         qp.end()
 
 
